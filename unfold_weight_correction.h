83d479711c940aacee30caf20bab8bfbcd6f7e47
denerslemos <denerslemos@gmail.com>
2024-04-09 09:11:34 -0500

    updates

--- unfold_weight_correction.h
+++ unfold_weight_correction.h
@@ -9,118 +9,118 @@ double getUnfCorrWeight(TFile *unffile, float leadpt, float subleadpt, int multi
   TH2 *eff_factor = nullptr; 
   if(multiplicity > 10.0 && multiplicity <= 60.0){
   	  if(etaregion == 0.5){
-		  unffile->GetObject("reco_ratio2D_10-60_mid-mid", eff_factor);  // data / MC
+		  unffile->GetObject("ratios/reco_ratio2D_10-60_mid-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 1.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_mid-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_mid-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 2.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_mid-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_mid-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 3.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_fwd-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_fwd-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 4.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_bkw-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_bkw-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 5.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_fwd-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_fwd-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 6.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_fwd-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_fwd-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 7.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_bkw-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_bkw-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 8.5){
- 		  unffile->GetObject("reco_ratio2D_10-60_bkw-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_10-60_bkw-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
 	  }
   } else if(multiplicity > 60.0 && multiplicity <= 120.0){
   	  if(etaregion == 0.5){
-		  unffile->GetObject("reco_ratio2D_60-120_mid-mid", eff_factor);  // data / MC
+		  unffile->GetObject("ratios/reco_ratio2D_60-120_mid-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 1.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_mid-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_mid-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 2.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_mid-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_mid-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 3.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_fwd-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_fwd-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 4.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_bkw-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_bkw-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 5.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_fwd-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_fwd-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 6.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_fwd-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_fwd-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 7.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_bkw-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_bkw-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 8.5){
- 		  unffile->GetObject("reco_ratio2D_60-120_bkw-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_60-120_bkw-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
 	  }
   } else if(multiplicity > 120.0 && multiplicity <= 185.0){
   	  if(etaregion == 0.5){
-		  unffile->GetObject("reco_ratio2D_120-185_mid-mid", eff_factor);  // data / MC
+		  unffile->GetObject("ratios/reco_ratio2D_120-185_mid-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 1.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_mid-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_mid-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 2.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_mid-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_mid-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 3.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_fwd-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_fwd-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 4.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_bkw-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_bkw-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 5.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_fwd-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_fwd-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 6.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_fwd-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_fwd-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 7.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_bkw-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_bkw-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 8.5){
- 		  unffile->GetObject("reco_ratio2D_120-185_bkw-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_120-185_bkw-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
 	  }
   } else if(multiplicity > 185.0 && multiplicity <= 250.0){
   	  if(etaregion == 0.5){
-		  unffile->GetObject("reco_ratio2D_185-250_mid-mid", eff_factor);  // data / MC
+		  unffile->GetObject("ratios/reco_ratio2D_185-250_mid-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 1.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_mid-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_mid-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 2.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_mid-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_mid-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 3.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_fwd-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_fwd-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 4.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_bkw-mid", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_bkw-mid", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 5.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_fwd-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_fwd-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 6.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_fwd-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_fwd-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 7.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_bkw-fwd", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_bkw-fwd", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
   	  } else if(etaregion == 8.5){
- 		  unffile->GetObject("reco_ratio2D_185-250_bkw-bkw", eff_factor);  // data / MC
+ 		  unffile->GetObject("ratios/reco_ratio2D_185-250_bkw-bkw", eff_factor);  // data / MC
   		  eff = eff_factor->GetBinContent( eff_factor->GetXaxis()->FindBin(leadpt),eff_factor->GetYaxis()->FindBin(subleadpt) );
 	  }
   }
